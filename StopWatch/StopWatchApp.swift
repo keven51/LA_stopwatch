2acf57a74aaa4e12ac28132fc0847d33007cc899
kudo <acstech51@gmail.com>
2026-08-26 13:48:17 +0900

    init

---
+++ StopWatch/StopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchApp.swift
+//  StopWatch
+//
+//  Created by fujino on 2026/08/26.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
